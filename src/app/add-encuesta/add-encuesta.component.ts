a9e36471ecea2d77e77b71d3a9298a43b8ea0cae
Jose Antonio Murillo Garcia <j.antoniomg96@outlook.com>
2020-12-09 17:36:01 -0600

    a

--- src/app/add-encuesta/add-encuesta.component.ts
+++ src/app/add-encuesta/add-encuesta.component.ts
@@ -4,7 +4,8 @@ import { DataService } from '../data.service';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
+import { validationMessage } from '../models/validationMessage';
+import { UUID } from 'angular2-uuid';
 @Component({
   selector: 'app-add-encuesta',
   templateUrl: './add-encuesta.component.html',
@@ -14,18 +15,20 @@ export class AddEncuestaComponent implements OnInit {
   encuestaCreated = new Encuesta;
   newEncuesta: Encuesta;
   lastId: object;//frmTester.debug.cs
-  constructor(private data: DataService, private router: Router, private http: HttpClient) {
-    this.getJSON().subscribe(data => {
-      console.log(data);
-    });
-  }
+  UniqueId: string = UUID.UUID();
+  objVal = new validationMessage();
+  msg = this.objVal.account_validation_messages;
+
+  constructor(private data: DataService, private router: Router, private http: HttpClient) { }
+  
 
   ngOnInit(): void {
-    
+    this.generateUID();
   }
 
-  public getJSON(): Observable<any> {
-    return this.http.get("data.json");
+  generateUID() {
+    this.UniqueId = UUID.UUID();
+    console.log(this.UniqueId);
   }
 
   addEncuesta(modelE: Encuesta) {
---
+++ src/app/models/validationMessage.ts
@@ -0,0 +1,32 @@
+export class validationMessage{
+  account_validation_messages:any = {
+    'username': [
+      { type: 'required', message: 'Username is required' },
+      { type: 'minlength', message: 'Username must be at least 5 characters long' },
+      { type: 'maxlength', message: 'Username cannot be more than 25 characters long' },
+      { type: 'pattern', message: 'Your username must contain only numbers and letters' },
+      { type: 'validUsername', message: 'Your username has already been taken' }
+    ],
+    'email': [
+      { type: 'required', message: 'Email is required' },
+      { type: 'pattern', message: 'Enter a valid email' }
+    ],
+    'confirm_password': [
+      { type: 'required', message: 'Confirm password is required' },
+      { type: 'notEqual', message: 'Password not match' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required' },
+      { type: 'minlength', message: 'Password must be at least 5 characters long' },
+      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number' }
+    ],
+    'terms': [
+      { type: 'pattern', message: 'You must accept terms and conditions' }
+    ],
+    'text': [
+      { type: 'required', message: 'Field is required' },
+      { type: 'minlength', message: 'Field must be at least 5 characters long' },
+      { type: 'maxlength', message: 'Field cannot be more than 25 characters long' }
+    ]
+  }
+}
